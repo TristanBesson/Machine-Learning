279e6d9a7a2e55f030a06e3c06e4252f8f2a6edb
SebastianSavi <sebastian.savidanniederer@epfl.ch>
2017-10-25 10:39:46 +0200

    Pseudo code for processing data

---
+++ scripts/preprocessing.py
@@ -0,0 +1,45 @@
+##Imports
+
+
+#Load data
+
+
+
+
+
+
+#Replace 999 of data with NaNs
+#Maybe evaluate % of NaNs (if to much NaNs -> discard column)
+
+
+
+
+
+#Standardize data by substracting to each colums its mean and dividing by standard deviation
+
+
+
+
+
+#Plot every feature according to an other (voir PDF lazare) to "visualize our data"
+
+
+
+
+
+
+#Do something about NaNs (a trouver: chercher dans les papers que faire quand ya des NaNs dans la data)
+
+
+
+
+
+
+#Evaluate variance to see most explaining data
+
+
+
+
+
+
+#Kernel trick or other tricks (polynomials)to augment the dimensions of data with best features couples (if strongly correlated: see from first figure)
