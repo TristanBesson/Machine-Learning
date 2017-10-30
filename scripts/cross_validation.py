28f332fb2bdb3f8df9c767b4720a76386355d792
jGsch <jean.gschwind@gmail.com>
2017-10-30 18:33:36 +0100

    comment cross valid + create the new plot of rmse

--- scripts/cross_validation.py
+++ scripts/cross_validation.py
@@ -3,14 +3,18 @@ from implementations import *
 import matplotlib.pyplot as plt
 
 def accuracy(y_predicted,y):
+    # Compute the accuracy of the predictions
     return 1 - sum(abs(y - y_predicted))/(2*len(y_predicted))
 
 def cross_validation(y, x, indices_k, k, lambda_, degree, model_function):
     """return the loss of ridge regression."""
+
+    # Create two different set of indice
     indice_test = indices_k[k]
     indice_train = indices_k[~(np.arange(indices_k.shape[0]) == k)]
     indice_train = indice_train.reshape(-1)
 
+    # Separate train and test data in function of the indice
     y_test = y[indice_test]
     y_train = y[indice_train]
     x_test = x[indice_test]
@@ -19,6 +23,8 @@ def cross_validation(y, x, indices_k, k, lambda_, degree, model_function):
     initial_w = np.zeros((x_train.shape[1], 1))
     max_iters = 500
     gamma = 0.01
+
+    # Choose the correct model
     model = model_function.__name__
 
     if model == "least_squares_GD" or model == "least_squares_SGD" or model == "logistic_regression":
@@ -30,15 +36,17 @@ def cross_validation(y, x, indices_k, k, lambda_, degree, model_function):
     else:
         w, loss = model_function(y_train, x_train, lambda_, initial_w, max_iters, gamma)
 
-    # calculate the loss for train and test data
+    # Compute loss for train and test data
     e_train = y_train - x_train.dot(w)
-    loss_train = np.sqrt(2 * compute_mse(e_train))
+    loss_train = np.sqrt(2*compute_mse(e_train))
     e_test = y_test - x_test.dot(w)
-    loss_test = np.sqrt(2 * compute_mse(e_test))
+    loss_test = np.sqrt(2*compute_mse(e_test))
 
-    y_pred = predict_labels(w,x_test)
-    acc = accuracy(y_pred,y_test)
-    print("Accuracy: ",acc)
+    # Compute the prediction and check the accuracy
+    y_pred = predict_labels(w, x_test)
+    acc = accuracy(y_pred, y_test)
+    print("Accuracy: ", acc)
+
     return loss_train, loss_test
 
 def build_k_indices(y, k_fold, seed):
@@ -47,23 +55,25 @@ def build_k_indices(y, k_fold, seed):
     interval = int(num_row / k_fold)
     np.random.seed(seed)
     indices = np.random.permutation(num_row)
-    k_indices = [indices[k * interval: (k + 1) * interval]
-                 for k in range(k_fold)]
+    k_indices = [indices[k * interval: (k + 1) * interval] for k in range(k_fold)]
     return np.array(k_indices)
 
 
 def find_best_lambda(y,x, degree, k_fold, model):
 
+    nb_lambdas = 20
     seed = 1
-    lambdas = np.logspace(-10, 0, 15)
+    best_lambda = 0
+    best_rmse_test = 100
 
+    # Create a space of lambdas to test based on a log rule
+    lambdas = np.logspace(-5, 0, nb_lambdas)
+    # Create different fold for the cross validation
     k_indices = build_k_indices(y, k_fold, seed)
 
     rmse_train = []
     rmse_test = []
 
-    best_lambda = 0
-    best_rmse_test = 100
 
     # cross validation
     for lambda_ in lambdas:
@@ -76,16 +86,26 @@ def find_best_lambda(y,x, degree, k_fold, model):
             rmse_train_tmp = np.mean(loss_train)
             rmse_test_tmp = np.mean(loss_test)
 
-            rmse_train.append(rmse_train_tmp)
-            rmse_test.append(rmse_test_tmp)
-
+            # We choose the smaller RMSE (for the test) to choose lambda
             if (best_rmse_test > rmse_test_tmp):
-                print(best_rmse_test,">", rmse_test_tmp, "= best lambda found", )
                 best_lambda = lambda_
                 best_rmse_test = rmse_test_tmp
                 best_rmse_train = rmse_train_tmp
 
-    print("\nBest lambda =", best_lambda, "\n")
+        rmse_train.append(rmse_train_tmp)
+        rmse_test.append(rmse_test_tmp)
 
+    ## Plot evolution of RMSE in function of lambdas
+    # plt.plot(rmse_train, label="rmse (train)")
+    # plt.plot(rmse_test, label="rmse (test)")
+    # plt.grid()
+    # plt.legend()
+    # plt.title("Root Mean Squared Error (RMSE) in function of lambda")
+    # plt.xlabel("lambda")
+    # plt.ylabel("RMSE")
+    # plt.xticks(range(0,nb_lambdas,1), np.round(lambdas,7), rotation=70)
+    # plt.show()
+
+    print("\nBest lambda =", best_lambda, "\n")
 
     return best_lambda, best_rmse_train, best_rmse_test
